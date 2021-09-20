538a5e1eb34729e98b56a3a56f2ea56be7f941f8
Yannick Schmetz <yannick.schmetz@inform-software.com>
2021-09-20 15:58:41 +0200

    Router

--- src/Router.ts
+++ src/Router.ts
@@ -1,5 +1,6 @@
 import {Application, Request, Response} from "express";
 import path from "path";
+import {QlikLdapLoginService} from "./QlikLdapLoginService";
 /**
  * Router that handels incoming requests.
  */
@@ -10,9 +11,31 @@ export class Router {
      */
     public static registerRoutes(app: Application): void {
         app.get("/", Router.getLoginMask);
+        app.post("/login", Router.login);
     }
 
     private static getLoginMask(req: Request, res: Response): void {
         res.sendFile(path.join(process.cwd(), "dist", "static", "index.html"));
+    }
+
+    private static login(req: Request, res: Response): void {
+        const connection = QlikLdapLoginService.getInstance().ldapConnection;
+        if (req.params.user === undefined || req.params.password === undefined) {
+            res.status(400);
+            res.send("Missing username or password!");
+            return;
+        }
+        connection
+            .checkUser(req.params.user, req.params.password)
+            .then(() => {
+                res.status(200);
+                res.send();
+                return;
+            })
+            .catch((err) => {
+                res.status(400);
+                res.send(err.message);
+                return;
+            });
     }
 }
